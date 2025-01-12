dc15c55f80949cfa99952ce79c835be0782b2e98
IceManRazer <42670833+IceManRazer@users.noreply.github.com>
2025-01-12 22:30:50 +0200

    Console Project Creation

---
+++ ConsoleManagementApp/ConsoleManagementApp/Program.cs
@@ -0,0 +1,7 @@
+﻿using System.Net.Http;
+
+HttpClient client = new HttpClient();
+
+var response = await client.GetAsync("https://localhost:44340/api/User/Login?username=Admin&password=terces");
+var content = await response.Content.ReadAsStringAsync();
+Console.WriteLine(content);

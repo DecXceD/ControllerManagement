c3c415f77488b24acfc1c5f6aa95af7c195e1d51
IceManRazer <42670833+IceManRazer@users.noreply.github.com>
2025-01-26 23:03:57 +0200

    Commands Added

--- ConsoleManagementApp/ConsoleManagementApp/Program.cs
+++ ConsoleManagementApp/ConsoleManagementApp/Program.cs
@@ -1,7 +1,49 @@
-﻿using System.Net.Http;
+﻿Console.WriteLine("Controller Management App");
+Console.WriteLine("Available commands:");
+Console.WriteLine("Login");
+Console.WriteLine("Logout");
+Console.WriteLine("Show Controllers");
+Console.WriteLine("Exit");
+string command = Console.ReadLine();
+switch(command){
+    case "Login":
+        await LoginAsync();
+        break;
 
-HttpClient client = new HttpClient();
+    case "Logout":
+        Logout();
+        break;
 
-var response = await client.GetAsync("https://localhost:44340/api/User/Login?username=Admin&password=terces");
-var content = await response.Content.ReadAsStringAsync();
-Console.WriteLine(content);
+    case "Show Controllers":
+        ShowControllers();
+        break;
+
+    case "Exit":
+        return;
+
+    default:
+        Console.WriteLine("Invalid Command");
+        break;
+}
+static async Task LoginAsync()
+{
+    Console.WriteLine("Enter Username:");
+    string username = Console.ReadLine();
+    Console.WriteLine("Enter Password:");
+    string password = Console.ReadLine();
+    HttpClient client = new HttpClient();
+
+    var response = await client.PostAsync($"https://localhost:44340/api/User/Login?username={username}&password={password}", null);
+    var content = await response.Content.ReadAsStringAsync();
+    Console.WriteLine(content);
+}
+
+static void Logout()
+{
+
+}
+
+static void ShowControllers()
+{
+
+}
